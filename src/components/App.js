838e2f824ec3dbfca1d277cd2fa125ef09cc695e
”sovsianykov” <swebproger@gmail.com>
2021-03-29 19:36:05 +0300

    three

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import { Button } from "react-bootstrap";
 import  firebase from "firebase/app";
 import 'firebase/database'
+import _ from 'lodash'
 
 
 
@@ -13,6 +14,43 @@ function App() {
     })
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [posts1, setPosts1] = useState([]);
+
+  useEffect(() =>{
+      firebase.database().ref('/posts').on('value', snapshot =>{
+         setPosts(snapshot.val())
+
+      })
+  },[firebase.database()])
+  const getPosts =() => {
+
+      firebase.database().ref('/posts').on('value', snapshot =>{
+      setPosts(snapshot.val())
+
+
+      })
+
+    console.log(posts)
+      let i = 0
+      let arr = []
+      for (var key in posts) {
+          arr[i] =  posts[key]
+          i++
+      }
+      console.log(arr)
+      setPosts1(arr)
+  }
+
+
+  const renderPosts = () => {
+      return _.map( posts , (key, post) => {
+           return (<div key={key} >
+               <h2>{post.title}</h2>
+               <p>{post.body}</p>
+           </div>)
+      } )
+    }
 
   const onImputChange = (e) => {
     setTitle(e.target.value);
@@ -27,6 +65,9 @@ function App() {
       body : body
     }
      firebase.database().ref('/posts').push(post)
+     setTitle('');
+     setBody('')
+
  }
 
   return (
@@ -34,6 +75,7 @@ function App() {
       <form onSubmit={onHandleSubmit}>
         <div className="form-group">
           <input
+              value={title}
             type="text"
             name="title"
             placeholder="Title"
@@ -43,6 +85,7 @@ function App() {
         </div>
         <div className="form-group">
           <input
+              value={body}
             type="body"
             name="body"
             placeholder="Body"
@@ -52,10 +95,21 @@ function App() {
         </div>
         <Button className="btn" onClick={onHandleSubmit}>Post</Button>
         <div>
-          <h2>{title}</h2>
-          <h2>{body}</h2>
+          {/*<h2>{title}</h2>*/}
+          {/*<h2>{posts.title}</h2>*/}
         </div>
       </form>
+        <Button className="btn" onClick={getPosts} >Get</Button>
+
+        <br/>
+        <div>
+            {posts1.map((el,i)=>{
+                return (<div key={i}>
+                    <h2>{el.title}</h2>
+                    <p> {el.body}</p>
+                </div>)
+            })}
+        </div>
     </div>
   );
 }
