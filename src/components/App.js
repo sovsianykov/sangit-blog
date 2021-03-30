28887893d187d77898a3ee235b9dc4a8f97846a9
sovsianykov <swebproger@gmail.com>
2021-03-30 09:01:03 +0300

    posts isworking

--- src/components/App.js
+++ src/components/App.js
@@ -1,117 +1,139 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
-import  firebase from "firebase/app";
-import 'firebase/database'
-import _ from 'lodash'
-
-
-
+import firebase from "firebase/app";
+import "firebase/database";
+import _ from "lodash";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import renderHtml from "react-render-html";
 
 function App() {
-    useEffect( () =>{
-        const db = firebase.database()
-         console.log(db)
-    })
+  useEffect(() => {
+    const db = firebase.database();
+    console.log(db);
+  });
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [posts, setPosts] = useState([]);
   const [posts1, setPosts1] = useState([]);
 
-  useEffect(() =>{
-      firebase.database().ref('/posts').on('value', snapshot =>{
-         setPosts(snapshot.val())
+  useEffect(() => {
+    firebase
+      .database()
+      .ref("/posts")
+      .on("value", (snapshot) => {
+        setPosts(snapshot.val());
+      });
+  }, [firebase.database()]);
+  const getPosts = () => {
+    firebase
+      .database()
+      .ref("/posts")
+      .on("value", (snapshot) => {
+        setPosts(snapshot.val());
+      });
 
-      })
-  },[firebase.database()])
-  const getPosts =() => {
-
-      firebase.database().ref('/posts').on('value', snapshot =>{
-      setPosts(snapshot.val())
-
-
-      })
-
-    console.log(posts)
-      let i = 0
-      let arr = []
-      for (var key in posts) {
-          arr[i] =  posts[key]
-          i++
-      }
-      console.log(arr)
-      setPosts1(arr)
-  }
-
-
-  const renderPosts = () => {
-      return _.map( posts , (key, post) => {
-           return (<div key={key} >
-               <h2>{post.title}</h2>
-               <p>{post.body}</p>
-           </div>)
-      } )
+    console.log(posts);
+    let i = 0;
+    let arr = [];
+    for (var key in posts) {
+      arr[i] = posts[key];
+      i++;
     }
+    console.log(arr);
+    setPosts1(arr);
+  };
 
-  const onImputChange = (e) => {
-    setTitle(e.target.value);
+  const onHandleChange = (e) => {
+     setBody(e)
   };
   const onImputBodyChange = (e) => {
     setBody(e.target.value);
   };
- const onHandleSubmit = (e) => {
-   e.preventDefault()
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
     const post = {
-     title : title,
-      body : body
-    }
-     firebase.database().ref('/posts').push(post)
-     setTitle('');
-     setBody('')
-
- }
+      title: title,
+      body: body,
+    };
+    firebase.database().ref("/posts").push(post);
+    setTitle("");
+    setBody("");
+  };
 
   return (
     <div className="container">
       <form onSubmit={onHandleSubmit}>
         <div className="form-group">
           <input
-              value={title}
+            value={title}
             type="text"
             name="title"
             placeholder="Title"
-            onChange={onImputChange}
+            onChange={ e=>{setTitle(e.target.value)}}
             className="form-control"
           />
         </div>
         <div className="form-group">
-          <input
-              value={body}
-            type="body"
-            name="body"
+          <ReactQuill
+            value={body}
+            modules={App.modules}
+            formats={App.formats}
             placeholder="Body"
-            onChange={onImputBodyChange}
-            className="form-control"
+            onChange={onHandleChange}
           />
         </div>
-        <Button className="btn" onClick={onHandleSubmit}>Post</Button>
+        <Button className="btn" onClick={onHandleSubmit}>
+          Post
+        </Button>
         <div>
           {/*<h2>{title}</h2>*/}
           {/*<h2>{posts.title}</h2>*/}
         </div>
       </form>
-        <Button className="btn" onClick={getPosts} >Get</Button>
+      <Button className="btn" onClick={getPosts}>
+        Get
+      </Button>
 
-        <br/>
-        <div>
-            {posts1.map((el,i)=>{
-                return (<div key={i}>
-                    <h2>{el.title}</h2>
-                    <p> {el.body}</p>
-                </div>)
-            })}
-        </div>
+      <br />
+      <div>
+        {posts1.map((el, i) => {
+          return (
+            <div key={i}>
+              <h2>{renderHtml(el.title)}</h2>
+              <p> {renderHtml(el.body)}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
+App.modules = {
+  toolbar: [
+    [{ header: 1 }, { header: 2 }, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "blockquote", "strike"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["clear"],
+    ["code-block"],
+  ],
+};
+App.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  " blockquote",
+  "strike",
+  "link",
+  "image",
+  "video",
+  "bullet",
+  "code-block",
+];
 
 export default App;
